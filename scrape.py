75660970ac961e3b720ed7512ea211c6942bd5cd
alexferrante <36389437+alexferrante@users.noreply.github.com>
2019-07-12 09:19:43 -0400

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,53 @@
+import requests
+import cld2
+import re
+import json
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from datetime import date, timedelta
+
+def init_values():
+      df = pd.DataFrame(columns=["id", "url", "headline", "tags"])
+      base = "https://medium.com/sitemap/posts/2019/posts-"
+
+      startDate = date(2019, 1, 1)
+      endDate = date.today() - timedelta(days=7)
+      daterange = pd.date_range(startDate, endDate)
+
+      for dt in daterange:
+            link = base + str(dt.strftime("%Y-%m-%d")) + ".xml"
+            res = requests.get(link)
+            soup = BeautifulSoup(res.content, "lxml")
+            for page in soup.find_all("loc"):
+                  if "@" not in page.get_text():
+                        article_title = page.get_text().rsplit('/', 1)[-1]
+                        stripped = re.sub("-\n|-|\n-", " ", article_title)
+                        if inspect_language(article_title):
+                              dfObj = populate_dataset(page.get_text())
+                              df = df.append(dfObj, ignore_index=True)
+                              print(df)
+
+def inspect_language(text):
+      isReliable, textBytesFound, details = cld2.detect(text)
+      if isReliable == 0:
+            return False
+      if "ENGLISH" in details[0] and details[0].percent > 94:
+            return True
+      else:
+            return False
+
+def populate_dataset(url):
+      # optimize this later, currently slow
+      res = requests.get(url)
+      soup = BeautifulSoup(res.content)
+      article_content = soup.find("article").get_text()
+      for meta_data in soup.findAll("script", attrs={"data-rh": "true", "type": "application/ld+json"}):
+            meta_data = json.loads(meta_data.get_text())
+            tags = list(filter(lambda x: "Tag" in x, meta_data["keywords"]))
+            tags = [tag.split(":")[1] for tag in tags]
+            if inspect_language(" ".join(str(x) for x in tags)) is False:
+                  continue
+            return {"id": meta_data["articleId"], "url": url, "headline": meta_data["headline"], "tags":tags, "content": article_content}
+
+init_values()
