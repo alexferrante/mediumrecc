8ea68cf429d91115bbee79c563f30461867612f6
alexferrante <36389437+alexferrante@users.noreply.github.com>
2019-07-13 14:31:58 -0400

    Add files via upload

---
+++ text_process.py
@@ -0,0 +1,23 @@
+from symspellpy.symspellpy import SymSpell 
+
+def remove_non_ascii(line):
+    return line.encode("ascii", errors="ignore").decode()
+
+def avg_word_length():
+    print()
+    
+def remove_common():
+    print()
+
+def remove_rare():
+    print()
+    
+def fix_punctuation():
+    print()
+
+def lemmatization():
+    print()
+
+
+
+print(remove_non_ascii('Hej då'))
